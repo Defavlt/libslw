5432b4170fe5d579955cf4563b21722c023c32de
Marcus Hansson <marcus.hansson@email.com>
2016-04-08 05:21:49 +0200

    Add static lua function/closure register handler

--- source/code/slw.cpp
+++ source/code/slw.cpp
@@ -2,3 +2,16 @@
 
 int
 Lua::State::handlers = 0;
+
+int
+Lua::State::handler( lua_State* state )
+{
+	Lua::State::EntryPoint* point = NULL;
+
+	point = 
+		( Lua::State::EntryPoint* ) 
+		( size_t )
+		( lua_tonumber( state, lua_upvalueindex( 1 ) ) );
+
+	return ( *point->entry )( *point->state, point->user );
+}

5bf00835c1f64df0282bf607832e2684210a5ff6
Marcus Hansson <marcus.hansson@email.com>
2020-10-18 22:43:01 +0300

    Add missing exception constructor implementations

---
+++ src/exception.cpp
@@ -0,0 +1,36 @@
+#include "slw/exception.hpp"
+#include "lua.hpp"
+
+slw::no_type_specialization::no_type_specialization()
+    : std::logic_error("missing, or no defined type template specialization")
+{
+}
+
+slw::unexpected_type::unexpected_type(shared_state  state)
+    : std::runtime_error("unexpected type encountered")
+{ luaL_error(state.get(), " unexpected type encountered");
+}
+
+slw::memory_allocation_failed::memory_allocation_failed()
+    : std::runtime_error("memory allocation failed")
+{
+}
+
+slw::error_error::error_error()
+    : std::runtime_error("error while handling an error")
+{
+}
+
+slw::unexpected_argument_range::unexpected_argument_range(
+        shared_state state
+        , slw::size_t got
+        , slw::size_t expected)
+    : std::runtime_error("expected "
+                         + std::to_string(expected)
+                         + ", got "
+                         + std::to_string(got))
+{ luaL_error(state.get(), (" expected "
+             + std::to_string(expected)
+             + " arguments, got "
+             + std::to_string(got)).c_str());
+}

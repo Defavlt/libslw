511becdfccb212718817c18c9a1eb12858337f5a
Marcus Hansson <marcus.hansson@email.com>
2020-10-18 22:33:22 +0300

    Add implementation of make_callable

--- src/functional.cpp
+++ src/functional.cpp
@@ -1,6 +1,18 @@
-#include "include/slw/functional.hpp"
+#include "slw/functional.hpp"
 
-functional::functional()
+#include <string>
+#include <iostream>
+
+int raw_call(lua_State *L)
 {
+    slw::callable *C = (slw::callable *)lua_touserdata(L, lua_upvalueindex(1));
+    return (*C)();
+}
 
+slw::reference slw::make_callable(slw::shared_state state, slw::table t, const slw::string_t &k, callable &f)
+{
+    lua_pushlightuserdata(state.get(), &f);
+    lua_pushcclosure(state.get(), raw_call, 1);
+    t.set(k, -1);
+    return t[k];
 }

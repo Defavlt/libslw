3f3463bf3748af9b51e89af0e3f6921249519a1d
Marcus Hansson <marcus.hansson@email.com>
2020-10-18 22:40:55 +0300

    Add missing reference implementation

---
+++ include/slw/reference.hpp
@@ -0,0 +1,199 @@
+#ifndef SLW_REFERENCE_HPP
+#define SLW_REFERENCE_HPP
+
+#include "state.hpp"
+#include "types.hpp"
+
+template<typename Tp>
+class variant;
+
+namespace slw {
+
+////////////////////////////////////////////////////////////
+/// \brief Create a reference to an object
+///
+///   State st;
+///   Reference age ("age");
+///   slw::type_e type = age.type();
+///   slw::int_t i = age.as<int>();
+///   bool is_number = age.is<slw::number_t>();
+///
+/// \see luaL_ref
+/// \see slw::Field
+////////////////////////////////////////////////////////////
+struct reference {
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Frees this reference.
+    ///
+    /// If it is the last reference to the object,
+    /// the object will be collected next the Lua garbage collector
+    /// is run.
+    ////////////////////////////////////////////////////////////
+    ~reference();
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a null reference
+    ////////////////////////////////////////////////////////////
+    reference(slw::shared_state state);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a reference to the object at the given, valid, index.
+    /// \param state The Lua state
+    /// \param idx The valid index
+    ////////////////////////////////////////////////////////////
+    reference(slw::shared_state state, slw::int_t idx);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a reference to the named object
+    ////////////////////////////////////////////////////////////
+    reference(slw::shared_state state, const slw::string_t &name);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a reference to the object, *in* the object
+    ///        at the given, valid index. E.g. get a reference to idx[name].
+    /// \param idx The valid index
+    /// \param name The name of the object
+    ////////////////////////////////////////////////////////////
+    reference(slw::shared_state state, slw::int_t idx, const slw::string_t &name);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Construct a reference to t[k]
+    /// \param t the valid containing object
+    /// \param k the key
+    ////////////////////////////////////////////////////////////
+    reference(slw::shared_state, slw::reference &t, const slw::string_t &k);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Copy constructor
+    ////////////////////////////////////////////////////////////
+    reference(const reference &);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Copy, and assign another reference to this reference,
+    /// releasing any already held references
+    ////////////////////////////////////////////////////////////
+    reference &operator =(reference);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Push to the stack, the object this reference references.
+    ////////////////////////////////////////////////////////////
+    void push();
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Assign the value at the top of the stack, to the object
+    ///        this reference, references.
+    ///
+    /// In other words, the sequence
+    /// \code
+    /// slw::reference::push();
+    /// slw::reference::assign();
+    /// \endcode
+    ///
+    /// is, basically, equivalent to `r = r`
+    ////////////////////////////////////////////////////////////
+    void assign();
+
+    reference operator [](const char *str)
+    { return operator [](slw::string_t { str });
+    }
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Array subscript for accessing fields on the object
+    /// this reference references.
+    /// \return A reference to the field.
+    ////////////////////////////////////////////////////////////
+    reference operator [](const slw::string_t &);
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Array subscript for accessing array fields on the object
+    /// this reference references.
+    /// \return A reference to the field.
+    ////////////////////////////////////////////////////////////
+    reference operator [](const slw::int_t &);
+
+    reference &operator =(reference &ref)
+    {
+        ref.push();
+        assign();
+        return *this;
+    }
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Is this a valid not-nil reference?
+    /// \return true if this is a valid reference
+    ////////////////////////////////////////////////////////////
+    bool valid();
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Compare to another reference
+    /// \return true if they're the same
+    /// \param rhs the other reference
+    ////////////////////////////////////////////////////////////
+    bool operator ==(const slw::reference &rhs) const;
+    bool operator !=(const slw::reference &rhs) const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the underlying state
+    /// \return slw::shared_state &
+    ////////////////////////////////////////////////////////////
+    slw::shared_state &get_state();
+    const slw::shared_state &get_state() const;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the type of the value referenced by this reference
+    /// \return slw::type_e the type of the value
+    ////////////////////////////////////////////////////////////
+    slw::type_e type();
+
+    ////////////////////////////////////////////////////////////
+    /// \brief Get the type of the value referenced by this reference
+    /// \return slw::type_e the type of the value
+    ////////////////////////////////////////////////////////////
+    template<type_e Te>
+    bool is() {
+        return Te == type();
+    }
+
+private:
+    ////////////////////////////////////////////////////////////
+    /// \brief The lua_State
+    ////////////////////////////////////////////////////////////
+    shared_state M_state;
+
+    ////////////////////////////////////////////////////////////
+    /// \brief The reference
+    ////////////////////////////////////////////////////////////
+    slw::int_t M_ref;
+};
+
+////////////////////////////////////////////////////////////
+/// \brief Make a reference to an object on the stack
+/// \param state The lua_State
+/// \param name The name of the field, e.g. person.age, or just age
+////////////////////////////////////////////////////////////
+reference make_reference(slw::shared_state state, const slw::string_t &name);
+}/*ns slw*/
+
+////////////////////////////////////////////////////////////
+/// \class slw::reference
+///
+/// slw::reference is a reference to an object in the Lua state.
+/// Upon destruction it automatically dereferences the object in question.
+///
+/// Usage example:
+/// \code
+/// // Declare, and create a Lua state
+/// slw::shared_state state = slw::make_state();
+/// // Allocate some data, e.g.
+/// // lua_dostring(state.get(), "jack = {}");
+/// // Make a reference to "jack"
+/// slw::reference c_jack = slw::make_reference(state, "jack");
+/// // Clear the global jack slot, e.g.
+/// //  lua_dostring(state.get(), "jack = nil");
+/// // Now "jack" will live on, at least until "c_jack" go out of scope
+/// \endcode
+///
+/// \see slw::shared_state, slw::make_state
+
+#endif // SLW_REFERENCE_HPP
--- src/reference.cpp
+++ src/reference.cpp
@@ -1,6 +1,176 @@
-#include "include/slw/reference.h"
+#include "slw/reference.hpp"
+#include "slw/get_field.hpp"
+#include "slw/variant/variant.hpp"
+#include "slw/variant/factory.hpp"
+#include <lua.hpp>
 
-Reference::Reference()
+#include <string>
+#include <iostream>
+
+void release(slw::shared_state &state, slw::int_t &ref)
 {
+    switch (ref) {
+    case LUA_NOREF:
+    case LUA_REFNIL:
+        return;
 
+    default:
+        luaL_unref(state.get(), slw::internal::indexes::registry, ref);
+    }
+}
+
+slw::int_t lock(slw::shared_state &state, const slw::string_t &name, slw::int_t idx)
+{
+    slw::internal::touch_path(state.get(), name, idx);
+    return luaL_ref(state.get(), slw::internal::indexes::registry);
+}
+
+slw::reference::~reference()
+{
+    release(M_state, M_ref);
+}
+
+slw::reference::reference(slw::shared_state state)
+    : M_state(state)
+    , M_ref(LUA_REFNIL)
+{
+    if (lua_gettop(M_state.get()))
+        M_ref = luaL_ref(state.get(), slw::internal::indexes::registry);
+}
+
+slw::reference::reference(slw::shared_state state, slw::int_t idx)
+    : M_state(state)
+    , M_ref(LUA_REFNIL)
+{
+    lua_pushvalue(state.get(), idx);
+    M_ref = luaL_ref(state.get(), slw::internal::indexes::registry);
+}
+
+slw::reference::reference(slw::shared_state state, const slw::string_t &name)
+    : M_state(state)
+    , M_ref(lock(state, name, slw::internal::indexes::globals))
+{
+}
+
+slw::reference::reference(slw::shared_state state, slw::int_t idx, const slw::string_t &name)
+    : M_state(state)
+    , M_ref(lock(state, name, idx))
+{
+}
+
+slw::reference::reference(slw::shared_state state, slw::reference &t, const slw::string_t &k)
+    : M_state(state)
+    , M_ref(LUA_REFNIL)
+{
+    if (t.valid()) {
+        t.push();
+        lua_createtable(M_state.get(), 0, 0);
+        lua_setfield(M_state.get(), -2, k.c_str());
+        lua_pop(M_state.get(), 1);
+    }
+}
+
+slw::reference::reference(const slw::reference &rhs)
+{
+    // an empty M_state means this is a 'null' reference
+    if (M_state.use_count())
+        release(M_state, M_ref);
+
+    M_state = rhs.M_state;
+    // borrow the other reference for a moment,
+    //  and create a new reference
+    M_ref = rhs.M_ref;
+    push();
+    M_ref = LUA_REFNIL;
+    assign();
+}
+
+slw::reference &slw::reference::operator =(slw::reference rhs)
+{
+    M_state = rhs.M_state;
+    rhs.push();
+    assign();
+    return *this;
+}
+
+void slw::reference::push()
+{
+    lua_rawgeti(M_state.get(), slw::internal::indexes::registry, M_ref);
+}
+
+void slw::reference::assign()
+{
+    release(M_state, M_ref);
+    M_ref = luaL_ref(M_state.get(), slw::internal::indexes::registry);
+}
+
+slw::reference slw::reference::operator [](const slw::string_t &path)
+{
+    push();
+    return slw::reference {
+        M_state,
+        -1,
+        path
+    };
+}
+
+slw::reference slw::reference::operator [](const slw::int_t &i)
+{
+    push();
+    lua_rawgeti(M_state.get(), -1, i);
+    lua_remove(M_state.get(), -2);
+    lua_pop(M_state.get(), 1);
+
+    return slw::reference { M_state, -1 };
+}
+
+bool slw::reference::valid()
+{
+    push();
+    bool is_nil =
+            (LUA_REFNIL != M_ref)
+            && (LUA_NOREF != M_ref)
+            && (LUA_TNIL != lua_type(M_state.get(), -1));
+    lua_pop(M_state.get(), 1);
+    return is_nil;
+}
+
+bool slw::reference::operator ==(const slw::reference &rhs) const
+{
+    slw::reference *p_rhs = const_cast<slw::reference *>(&rhs);
+    slw::reference *p_lhs = const_cast<slw::reference *>(this);
+
+    p_rhs->push();
+    p_lhs->push();
+
+    bool eq = lua_compare(p_lhs->M_state.get(), -1, -2, LUA_OPEQ);
+    lua_pop(p_lhs->M_state.get(), 2);
+    return eq;
+}
+
+bool slw::reference::operator !=(const slw::reference &rhs) const
+{ return !(*this == rhs);
+}
+
+slw::shared_state &slw::reference::get_state()
+{
+    return M_state;
+}
+
+const slw::shared_state &slw::reference::get_state() const
+{
+    return M_state;
+}
+
+slw::type_e slw::reference::type()
+{
+    push();
+    slw::type_e type = (slw::type_e)lua_type(M_state.get(), -1);
+    lua_pop(M_state.get(), 1);
+    return type;
+}
+
+slw::reference slw::make_reference(slw::shared_state state, const slw::string_t &name)
+{
+    return slw::reference { state, name };
 }
